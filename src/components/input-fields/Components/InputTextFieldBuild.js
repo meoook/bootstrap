b18a0a6cf095e162cb6b023f41f0c83d09ba2688
meok-mac <mewooook@gmail.com>
2020-09-20 15:50:12 +0300

    ok

--- src/components/input-fields/Components/InputTextFieldBuild.js
+++ src/components/input-fields/Components/InputTextFieldBuild.js
@@ -1,40 +1,46 @@
-import React, { useEffect, useRef, useState } from "react"
-import InputFieldButtons from "./InputFieldButtons"
-import InputFieldIcon from "./InputFieldIcon"
-import InputFieldOutline from "./InputFieldOutline"
-import InputFieldValues from "./InputFieldValues"
-import InputFieldChoices from "./InputFieldChoices"
-import InputFieldError from "./InputFieldError"
+import React, { useEffect, useRef, useState } from 'react'
+import InputFieldButtons from './InputFieldButtons'
+import InputFieldIcon from './InputFieldIcon'
+import InputFieldOutline from './InputFieldOutline'
+import InputFieldValues from './InputFieldValues'
+import InputFieldChoices from './InputFieldChoices'
+import InputFieldError from './InputFieldError'
 
 // Util
-const filterChoices = (value, choices) => choices.filter((item) => item.toLowerCase().includes(value.toLowerCase()))
+const filterChoices = (value, choices) =>
+  choices.filter((item) => item.toLowerCase().includes(value.toLowerCase()))
 
 const InputTextFieldBuild = ({
-  val, // Value setted by program
-  chVal, // Handle change value
+  val = '', // Value setted by program
+  chVal = null, // Handle change value
   vals = [], // Display multi values
   rmVal = null, // Callback: To remove value from values list
-  label = "", // label with outline or placeholder
-  ph = "Введите значение", // Placeholder (displayed only with choices or with icon field)
-  icon = "", // icon name before text
-  errorText = "", // error text (out - red)
-  helpText = "", // help text onHover info Icon
-  outColor = "", // if set - display outline border & label control
+  label = '', // label with outline or placeholder
+  ph = 'Введите значение', // Placeholder (displayed only with choices or with icon field)
+  icon = '', // icon name before text
+  errorText = '', // error text (out - red)
+  helpText = '', // help text onHover info Icon
+  outColor = '', // if set - display outline border & label control
   onFinish = null, // Callback: reset or onBlur or keyEnter or select from choices
   choices = [], // Choices for dropdown list, if not set - display "no variants"
   loading = false, // State for field loading (choices and icon)
   disabled = false, // disable ui control
 }) => {
   // State
-  const [inputVal, setInputVal] = useState("")
+  const [inputVal, setInputVal] = useState('')
   const inRef = useRef()
   const choicesRef = useRef([]) // Must be an array of special items
   const dChoicesRef = useRef(false) // Toggler to display choices
 
   // Const state
   const required = icon || vals.length ? false : true
-  const placeholder = !required || !label ? ph : ""
-  const btnClass = loading && (helpText || choices) ? " btns" : loading || helpText || choices ? " btn" : ""
+  const placeholder = !required || !label ? ph : ''
+  const btnClass =
+    loading && (helpText || choices)
+      ? ' btns'
+      : loading || helpText || choices
+      ? ' btn'
+      : ''
   // Listners
   useEffect(() => {
     setInputVal(val)
@@ -43,29 +49,29 @@ const InputTextFieldBuild = ({
 
   // Set window listner cos e.stopPropagation don't work with onBlur
   useEffect(() => {
-    window.addEventListener("click", handleMouseOver)
+    window.addEventListener('click', handleMouseOver)
     return () => {
-      window.removeEventListener("click", handleMouseOver)
+      window.removeEventListener('click', handleMouseOver)
     }
     // eslint-disable-next-line
   }, [])
 
   // Handlers
   const handleClick = (e) => {
-    console.log("Check focused")
+    console.log('Check focused')
     e.stopPropagation()
     if (disabled) return
     if (inRef.current !== document.activeElement) {
-      console.log("Not focused - focusing")
+      console.log('Not focused - focusing')
       inRef.current.focus()
     } else if (choices.length) {
-      console.log("Focused witch choices - toggle")
+      console.log('Focused witch choices - toggle')
       dChoicesRef.current = !dChoicesRef.current
     }
   }
 
   const handleChange = (e) => {
-    console.log("Changing input")
+    console.log('Changing input')
     if (disabled) return
     const value = e.target.value
     setInputVal(value)
@@ -74,29 +80,29 @@ const InputTextFieldBuild = ({
   }
 
   const handleKeyPress = (e) => {
-    console.log("Key press")
+    console.log('Key press')
     if (disabled) return
-    if (onFinish && e.key === "Enter") onFinish(inputVal.trim())
+    if (onFinish && e.key === 'Enter') onFinish(inputVal)
   }
 
   const handleReset = (e) => {
-    console.log("Reset")
+    console.log('Reset')
     e.stopPropagation()
     if (disabled) return
-    if (onFinish) onFinish("")
-    else setInputVal("")
+    if (onFinish) onFinish('')
+    else setInputVal('')
     if (choices.length) choicesRef.current = choices
   }
 
   const handleChoicesToggle = (e) => {
-    console.log("Choices toggle")
+    console.log('Choices toggle')
     e.stopPropagation()
     if (disabled) return
     dChoicesRef.current = !dChoicesRef.current
   }
 
   const handleChoiceClick = (value, e) => {
-    console.log("Choice selected")
+    console.log('Choice selected')
     e.stopPropagation()
     if (onFinish) onFinish(value)
     else setInputVal(value)
@@ -104,21 +110,21 @@ const InputTextFieldBuild = ({
   }
 
   const handleRemoveVal = (value, e) => {
-    console.log("Remove value")
+    console.log('Remove value')
     e.stopPropagation()
     if (disabled) return
     if (rmVal) rmVal(value)
   }
 
   const handleMouseOver = () => {
-    console.log("Mouse over")
+    console.log('Mouse over')
     if (disabled) return
-    if (onFinish) onFinish(inputVal.trim())
+    if (onFinish) onFinish(inputVal)
   }
 
   return (
     <div className='input' onClick={handleClick}>
-      <div className={`input-root${icon ? " icon" : ""}${btnClass}`}>
+      <div className={`input-root${icon ? ' icon' : ''}${btnClass}`}>
         <InputFieldValues values={vals} removeVal={handleRemoveVal} />
         <input
           tabIndex='0'
@@ -144,7 +150,12 @@ const InputTextFieldBuild = ({
           loading={loading}
           disabled={disabled}
         />
-        <InputFieldOutline label={label} color={outColor} loading={loading} disabled={disabled} />
+        <InputFieldOutline
+          label={label}
+          color={outColor}
+          loading={loading}
+          disabled={disabled}
+        />
       </div>
       <InputFieldError text={errorText} /> ||
       <InputFieldChoices
--- src/components/input-fields/InputText.js
+++ src/components/input-fields/InputText.js
@@ -1,12 +1,10 @@
-import React from "react"
-import InputTextFieldWrapper from "./Components/InputTextFieldWrapper"
+import React, { useRef } from 'react'
+import InputTextFieldBuild from './Components/InputTextFieldBuild'
 
 const InputText = (props) => {
-  return 
-    <InputTextFieldWrapper>
-    <
-
-    </InputTextFieldWrapper>
+  const val = useRef('init val')
+  const finish = (value) => (val.current = value.trim())
+  return <InputTextFieldBuild val={val.current} onFinish={finish} />
 }
 
 export default InputTextFieldBuild
